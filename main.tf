d487d54abafc370586c263d72903a3a78ee44d73
vikram sardeshpande <vikram@psl.com>
2018-01-31 17:19:59 +0530

    modified security group for prod-web

--- main.tf
+++ main.tf
@@ -44,6 +44,12 @@ resource "aws_security_group" "default" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   # HTTP access from the VPC
   ingress {
