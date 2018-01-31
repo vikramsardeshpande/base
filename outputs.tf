c2aa471bee57736264fa1812ca323c22ecc7663b
vikram sardeshpande <vikram@psl.com>
2018-01-31 14:08:34 +0530

    initial commit

---
+++ outputs.tf
@@ -0,0 +1,11 @@
+output "default_vpc_id" {
+  value = "${aws_vpc.default.id}"
+}
+
+output "default_security_group_id" {
+  value = "${aws_security_group.default.id}"
+}
+
+output "default_subnet_id" {
+  value = "${aws_subnet.default.id}"
+}
